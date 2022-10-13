5049fa48a1a461fd311e634eabccac5d2a3c3510
WP-Developer <55971399+wp-deepak@users.noreply.github.com>
2022-10-13 20:14:48 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,13 +1,11 @@
 jQuery(document).ready(function() {
-
-	/*	1. Adding active class current menu items. */
+	
+	/*1. Adding active class current menu items. */
+	
 	jQuery('nav li a').filter(function(){
-
-        return this.href === location.href;
-
-        jQuery('nav li').removeClass("active");  
-
-    }).addClass('active');
+       		 return this.href === location.href;
+      		  jQuery('nav li').removeClass("active");  
+   	 }).addClass('active');
 
 
 
